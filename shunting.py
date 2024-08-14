0f540f025ed7500be3c61d35c9e8de3f3695a6fd
Hamburger317 <107779558+Hamburger317@users.noreply.github.com>
2024-08-13 18:15:17 -0700

    3 clean up code (#4)
    * Simplified operator logic in `postfix`
    Inverted `if operators` as a guard clause.
    Simplified the conditional expressions into helper functions `_higher_priority` and `_top_is_open_parenthesis`.
    
    * Simplified code in `solve_postfix`
    Added helper functions `_evaluate_operator` and `_evaluate_unary`.
    Separated the core logic and the evaluation logic in `solve_postfix`.
    
    * Improved typing
    Added type hints to helpers and added `Stack` type alias.

--- shunting.py
+++ shunting.py
@@ -1,17 +1,50 @@
+from typing import TypeAlias
+
 from lexer import Category, Token
 from operators import Associativity, Operator
 
 
-def postfix(expression: list[Token]) -> list[Token]:
-    output: list[Token] = []
-    operators: list[Token] = []
+_OPERATORS = (Category.OPERATOR, Category.UNARY_OPERATOR)
+
+Stack: TypeAlias = list[Token]
+
+
+def _higher_priority(operator: Operator, other: Operator) -> bool:
+    return operator.precedence > other.precedence or (
+        operator.precedence == other.precedence
+        and other.associativity == Associativity.LEFT
+    )
+
+
+def _top_is_open_parenthesis(stack: Stack) -> bool:
+    return stack[-1].category == Category.PARENTHESIS_OPEN
+
+
+def _handle_operator(token: Token, output: Stack, operator_stack: Stack):
+    assert isinstance(token.value, Operator)
+
+    operator = token.value
+
+    while (
+        operator_stack
+        and not _top_is_open_parenthesis(operator_stack)
+        and _higher_priority(operator_stack[-1].value, operator)
+    ):
+        output.append(operator_stack.pop())
+
+    operator_stack.append(token)
+
+
+def postfix(expression: Stack) -> Stack:
+    output: Stack = []
+    operators: Stack = []
 
     for token in expression:
         if token.category == Category.NUMBER:
             output.append(token)
 
         elif token.category == Category.PARENTHESIS_CLOSE:
-            while operators and operators[-1].category != Category.PARENTHESIS_OPEN:
+            while operators and not _top_is_open_parenthesis(operators):
                 output.append(operators.pop())
 
             operators.pop()
@@ -19,33 +52,11 @@ def postfix(expression: list[Token]) -> list[Token]:
         elif token.category == Category.PARENTHESIS_OPEN:
             operators.append(token)
 
-        elif (
-            token.category == Category.OPERATOR
-            or token.category == Category.UNARY_OPERATOR
-        ):
-            assert isinstance(token.value, Operator)
-            operator = token.value
-
-            if operators:
-                top = operators[-1]
-                top_operator = top.value
-
-                while (
-                    top.category != Category.PARENTHESIS_OPEN
-                    and top_operator.precedence > operator.precedence
-                    or (
-                        top.category != Category.PARENTHESIS_OPEN
-                        and operator.precedence == top_operator.precedence
-                        and top_operator.associativity == Associativity.LEFT
-                    )
-                ):
-                    output.append(top)
-                    top = operators.pop()
-
-                    if not operators or top.category:
-                        break
-
+        elif token.category in _OPERATORS and not operators:
             operators.append(token)
+
+        elif token.category in _OPERATORS:
+            _handle_operator(token, output, operators)
 
     while operators:
         output.append(operators.pop())
@@ -53,7 +64,35 @@ def postfix(expression: list[Token]) -> list[Token]:
     return output
 
 
-def solve_postfix(postfix_tokens: list[Token]) -> float:
+def _evaluate(operand: float, other: float, operator: Operator) -> float:
+    if operator == "+":
+        return operand + other
+
+    elif operator == "-":
+        return operand - other
+
+    elif operator == "*":
+        return operand * other
+
+    elif operator == "/":
+        return operand / other
+
+    elif operator == "%":
+        return int(operand) % int(other)
+
+    elif operator == "^":
+        return operand**other
+
+
+def _evaluate_unary(operand: float, operator: Stack) -> float:
+    if operator == "+":
+        return +operand
+
+    elif operator == "-":
+        return -operand
+
+
+def solve_postfix(postfix_tokens: Stack) -> float:
     solve_stack: list[float] = []
 
     for token in postfix_tokens:
@@ -62,59 +101,20 @@ def solve_postfix(postfix_tokens: list[Token]) -> float:
             solve_stack.append(token.value)
 
         elif token.category == Category.OPERATOR:
-            assert isinstance(token.value, Operator)
-            # Operands flipped due to popping.
-            if token.symbol == "+":
-                assert len(solve_stack) >= 2
-                operand1 = solve_stack.pop()
-                operand2 = solve_stack.pop()
+            assert len(solve_stack) >= 2
 
-                solve_stack.append(operand2 + operand1)
+            operand2 = solve_stack.pop()
+            operand = solve_stack.pop()
 
-            elif token.symbol == "-":
-                assert len(solve_stack) >= 2
-                operand1 = solve_stack.pop()
-                operand2 = solve_stack.pop()
-
-                solve_stack.append(operand2 - operand1)
-
-            elif token.symbol == "*":
-                assert len(solve_stack) >= 2
-                operand1 = solve_stack.pop()
-                operand2 = solve_stack.pop()
-
-                solve_stack.append(operand2 * operand1)
-
-            elif token.symbol == "/":
-                assert len(solve_stack) >= 2
-                operand1 = solve_stack.pop()
-                operand2 = solve_stack.pop()
-
-                solve_stack.append(operand2 / operand1)
-
-            elif token.symbol == "%":
-                assert len(solve_stack) >= 2
-                operand1 = int(solve_stack.pop())
-                operand2 = int(solve_stack.pop())
-
-                solve_stack.append(operand2 % operand1)
-
-            elif token.symbol == "^":
-                assert len(solve_stack) >= 2
-                operand1 = solve_stack.pop()
-                operand2 = solve_stack.pop()
-
-                solve_stack.append(operand2**operand1)
+            result = _evaluate(operand, operand2, token.symbol)
+            solve_stack.append(result)
 
         elif token.category == Category.UNARY_OPERATOR:
-            if token.symbol == "-":
-                operand = solve_stack.pop()
+            assert len(solve_stack) >= 1
 
-                solve_stack.append(-operand)
+            operand = solve_stack.pop()
 
-            elif token.symbol == "+":
-                operand = solve_stack.pop()
+            result = _evaluate_unary(operand, token.symbol)
+            solve_stack.append(result)
 
-                solve_stack.append(+operand)
-
-    return float(solve_stack[0])
+    return solve_stack[0]
